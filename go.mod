4f743f9a5a6a0657f638dc7fc79a93ec5d0b6244
Adam Carter <adamcarter@jakatasalon.co.uk>
2019-12-12 12:11:00 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module contra-design.com/javascript_practice
+
+go 1.12
+
+require github.com/gorilla/mux v1.7.3
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tplIndex *template.Template
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := tplIndex.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	var err error
+
+	port := "8080"
+
+	tplIndex = template.Must(template.ParseFiles(
+		"views/index.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", index).Methods("GET")
+
+	// Assett Handler
+	assetHandler := http.FileServer(http.Dir("./dist/"))
+	assetHandler = http.StripPrefix("/dist/", assetHandler)
+	r.PathPrefix("/dist/").Handler(assetHandler)
+
+	log.Printf("Starting server on %s", port)
+
+	http.ListenAndServe(":" + port, r)
+}
